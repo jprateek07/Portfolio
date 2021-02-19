5ce8c8f82d4c64c5a30ba86da2ae1118ee0315f1
prateek jain <jprateek07@gmail.com>
2021-02-19 16:07:21 +0530

    mobile responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,22 @@
-// $('.counter-count').each(function () {
-//         $(this).prop('Counter',0).animate({
-//             Counter: $(this).text()
-//         }, {
-//             duration: 5000,
-//             easing: 'swing',
-//             step: function (now) {
-//                 $(this).text(Math.ceil(now));
-//             }
-//         });
-//     });
-// function Circlle(el){
-//     $(el).circleProgress({fill:{color:'#ff5c5c'}})
-//     .on('circle-animation-progress',function(event,progress,stepValue){
-//         $(this).find('strong').text(String(stepValue.toFixed(2)).substr(2)+'%')
-//     });
-// }
-// Circlle('.round');
-function Circlle(el){
-    $(el).circleProgress({fill: {color: '#ff5c5c'}})
-      .on('circle-animation-progress', function(event, progress, stepValue){
-                      $(this).find('strong').text(String(stepValue.toFixed(2)).substr(2)+'%');
-              });  
-  };
-  Circlle('.round');
+function Circlle(el) {
+  $(el).circleProgress({
+      fill: {
+        color: '#ff5c5c'
+      },
+      size: 100,
+    })
+    .on('circle-animation-progress', function (event, progress, stepValue) {
+      $(this).find('strong').text(String(stepValue.toFixed(2)).substr(2) + '%');
+    });
+};
+Circlle('.round');
+$('#myCarousel').on('slide.bs.carousel', function onSlide(ev) {
+  var id = ev.relatedTarget.id;
+  if ($('div.effect').hasClass(id)) {
+    document.getElementsByClassName(id)[0].style.animation = "ClientPopUp 2s infinite"
+    setTimeout(function () {
+      document.getElementsByClassName(id)[0].style.animation = "PopUpEffect 4s infinite"
+    }, 2000);
+  }
+
+})
